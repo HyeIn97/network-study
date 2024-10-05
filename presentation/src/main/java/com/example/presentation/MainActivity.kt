69a3996e1f3fa1f9b78f1fd00e1a6f3e4740f799
HyeIn97 <wslx97@naver.com>
2024-10-05 18:19:11 +0900

    [feat] 화면 구성을 위해 tabLayout과 fragment adatper를 초기화한다.

--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.presentation
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.example.presentation.adapter.ViewPagerAdapter
 import com.example.presentation.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,5 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this), null, false)
         setContentView(binding.root)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() = with(binding) {
+        viewPager.adapter = ViewPagerAdapter(this@MainActivity)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.search_tab)
+                else -> tab.text = getString(R.string.like_tab)
+            }
+        }.attach()
     }
 }
