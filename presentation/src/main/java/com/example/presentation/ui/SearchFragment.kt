8d88610f28436a11f2c4c21282260acf3624abab
HyeIn97 <wslx97@naver.com>
2024-10-14 23:30:29 +0900

    [refactor] 생성한 baseFragment를 사용해준다.

--- presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
@@ -1,8 +1,12 @@
 package com.example.presentation.ui
 
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchFragment : Fragment(){
+class SearchFragment() : BaseFragment<FragmentSearchBinding>() {
+    override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) = FragmentSearchBinding.inflate(inflater, container, false)
 }
