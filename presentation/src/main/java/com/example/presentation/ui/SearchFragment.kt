68c3f3599e5da348ca49d50481222ece01dd8ecd
HyeIn97 <wslx97@naver.com>
2024-10-15 22:52:11 +0900

    [feat] searchFragment를 생성한다.

--- presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
@@ -1,12 +1,45 @@
 package com.example.presentation.ui
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentSearchBinding
+import com.example.presentation.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment() : BaseFragment<FragmentSearchBinding>() {
+    private val viewModel: SearchViewModel by viewModels()
     override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) = FragmentSearchBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifeCycleScope()
+        listener()
+        initAdapter()
+    }
+
+    private fun lifeCycleScope() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+        }
+    }
+
+    private fun listener() = with(binding) {
+        searchButton.setOnClickListener {
+
+        }
+    }
+
+    private fun initAdapter() = with(binding) {
+
+    }
 }
