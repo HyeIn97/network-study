b6f2cb513f10fc787b1f22546a1aa8b4ee67d10a
HyeIn97 <wslx97@naver.com>
2024-10-16 21:35:48 +0900

    [refactor] try-catch를 이용해 binding이 실패했을 경우 empty ui를 표기한다.

--- presentation/src/main/java/com/example/presentation/base/BaseFragment.kt
+++ presentation/src/main/java/com/example/presentation/base/BaseFragment.kt
@@ -6,23 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.example.presentation.databinding.UiBindingExpectationBinding
+import com.example.presentation.R
 
 abstract class BaseFragment<T : ViewBinding> : Fragment() {
     private var _binding: T? = null
-    val binding get() = _binding !!
+    val binding get() = _binding ?: throw IllegalStateException("base fragment binding error")
 
     abstract fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): T
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = getViewBinding(inflater, container)
-        return binding.root
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        try {
+            _binding = getViewBinding(inflater, container)
+            binding.root
+        } catch (e: Exception) {
+            inflater.inflate(R.layout.ui_binding_expectation, container, false)
+        }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
     }
-
-    private fun bindingExpectation() = UiBindingExpectationBinding.inflate(layoutInflater, null, false)
 }
