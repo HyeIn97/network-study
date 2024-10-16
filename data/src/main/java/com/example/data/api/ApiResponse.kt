60eb8799cda847db092088217f54963eaf50cd49
HyeIn97 <wslx97@naver.com>
2024-10-16 21:38:36 +0900

    [feat] 응답 값을 presentation 모듈로 전달하기 위해 flow로 변환 시켜준다.

--- data/src/main/java/com/example/data/api/ApiResponse.kt
+++ data/src/main/java/com/example/data/api/ApiResponse.kt
@@ -16,4 +16,26 @@ data class NaverApi<T>(
     val response: NaverResponse,
     val items: ArrayList<T>
 )
+
+fun <T> apiCallToFlow(data: suspend () -> Response<NaverApi<T>>): Flow<NaverApi<T>> = flow {
+    try {
+        val response = data()
+        if (response.isSuccessful) {
+            val apiResponse = response.body()
+            if (apiResponse != null) {
+                emit(apiResponse)
+            } else {
+                throw NullPointerException("Response body is null")
+            }
+        } else {
+            val errorBody = response.errorBody()?.string()
+            if (errorBody != null) {
+                throw HttpException(response)
+            } else {
+                throw HttpException(response)
+            }
+        }
+    } catch (e: IOException) {
+        throw e
+    }
 }
