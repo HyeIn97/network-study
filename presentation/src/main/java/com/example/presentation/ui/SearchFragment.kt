e0cf042d7152aaa02f7d6cd7ca489ab8e4316dea
HyeIn97 <wslx97@naver.com>
2024-10-17 17:35:03 +0900

    [feat] 성공적으로 검색 결과를 가져왔을 경우 ui에 적용한다.

--- presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/SearchFragment.kt
@@ -1,14 +1,18 @@
 package com.example.presentation.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.model.SearchModel
+import com.example.presentation.adapter.BookAdapter
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentSearchBinding
 import com.example.presentation.viewmodel.SearchViewModel
@@ -29,9 +33,9 @@ class SearchFragment() : BaseFragment<FragmentSearchBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifeCycleScope()
         listener()
         initAdapter()
+        lifeCycleScope()
     }
 
     private fun lifeCycleScope() = lifecycleScope.launch {
@@ -39,7 +43,33 @@ class SearchFragment() : BaseFragment<FragmentSearchBinding>() {
             launch {
                 viewModel.search.collect {
                     it?.let {
-                        Log.d("debug", "결과 값  : $it")
+                        binding.run {
+                            if (it.total > 0) {
+                                if (viewModel.initQuery) {
+                                    countText.text = it.total.toString()
+
+                                    countLayout.visibility = View.VISIBLE
+                                    binding.emptyText.visibility = View.GONE
+                                    binding.bookRecycler.visibility = View.VISIBLE
+
+                                    bookList.clear()
+                                    bookList.addAll(it.bookList)
+                                    bookAdapter.notifyDataSetChanged()
+
+                                    viewModel.totalCount = it.total
+                                    viewModel.initQuery = false
+                                } else {
+                                    bookList.addAll(it.bookList)
+                                    bookAdapter.notifyItemRangeInserted(bookList.lastIndex - 9, bookList.lastIndex)
+                                }
+                            } else {
+                                bookList.clear()
+
+                                countLayout.visibility = View.GONE
+                                binding.emptyText.visibility = View.VISIBLE
+                                binding.bookRecycler.visibility = View.GONE
+                            }
+                        }
                     }
                 }
             }
