fc5eb7dc12cabe88806bb6bcef9a100929e45bcc
HyeIn97 <wslx97@naver.com>
2024-10-29 15:55:40 +0900

    [feat] 좋아요 목록 dataSource를 구현한다.

--- data/src/main/java/com/example/data/proto/datasource/LikeDataSource.kt
+++ data/src/main/java/com/example/data/proto/datasource/LikeDataSource.kt
@@ -1,4 +1,10 @@
 package com.example.data.proto.datasource
 
+import com.example.domain.proto.model.LikeModel
+import kotlinx.coroutines.flow.Flow
+
 interface LikeDataSource {
+    fun getLike(): Flow<ArrayList<LikeModel>>
+    suspend fun setLike(like: LikeModel)
+    suspend fun deleteLike(like: LikeModel)
 }
--- data/src/main/java/com/example/data/proto/serializer/LikeSerializer.kt
+++ data/src/main/java/com/example/data/proto/serializer/LikeSerializer.kt
@@ -3,7 +3,7 @@ package com.example.data.proto.serializer
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
 import com.example.data.LikePreferences
-import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 
