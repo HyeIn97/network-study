04ccc2810e32282fa8aedf646604c8c29c58241f
HyeIn97 <wslx97@naver.com>
2024-11-01 10:12:18 +0900

    [feat] 좋아요 viewModel을 생성한다.

--- presentation/src/main/java/com/example/presentation/ui/fragment/LikeFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/fragment/LikeFragment.kt
@@ -2,11 +2,14 @@ package com.example.presentation.ui.fragment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentLikeBinding
+import com.example.presentation.viewmodel.LikeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LikeFragment : BaseFragment<FragmentLikeBinding>() {
+    private val viewModel: LikeViewModel by viewModels()
     override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) = FragmentLikeBinding.inflate(inflater, container, false)
 }
