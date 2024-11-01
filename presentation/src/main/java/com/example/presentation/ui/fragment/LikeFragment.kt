d5f9d8032f20c147a926fcdf6ccc153c9a7e3a71
HyeIn97 <wslx97@naver.com>
2024-11-01 10:35:10 +0900

    [feat] stateFlow를 이용해 좋아요 리스트 흐름을 생성해주고 fragment에서 사용한다.

--- presentation/src/main/java/com/example/presentation/ui/fragment/LikeFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/fragment/LikeFragment.kt
@@ -1,15 +1,56 @@
 package com.example.presentation.ui.fragment
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentLikeBinding
 import com.example.presentation.viewmodel.LikeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LikeFragment : BaseFragment<FragmentLikeBinding>() {
     private val viewModel: LikeViewModel by viewModels()
     override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?) = FragmentLikeBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getLike()
+        lifeCycleScope()
+    }
+
+    private fun lifeCycleScope() = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            launch {
+                viewModel.like.collect {
+                    if (it.size > 0) {
+                        binding.likeRecycler.visibility = View.VISIBLE
+                        binding.emptyTxt.visibility = View.GONE
+
+                        hasLike()
+                    } else {
+                        binding.likeRecycler.visibility = View.GONE
+                        binding.emptyTxt.visibility = View.VISIBLE
+
+                        notLike()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hasLike() {
+
+    }
+
+    private fun notLike() {
+
+    }
 }
--- presentation/src/main/java/com/example/presentation/viewmodel/LikeViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/LikeViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.proto.model.LikeModel
+import com.example.domain.proto.usecase.GetLikeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LikeViewModel @Inject constructor() : ViewModel() {
+class LikeViewModel @Inject constructor(private val getLikeUseCase: GetLikeUseCase) : ViewModel() {
+    private val _like = MutableStateFlow<ArrayList<LikeModel>>(arrayListOf())
+    val like = _like.asStateFlow()
+
+    fun getLike() = viewModelScope.launch {
+        _like.emit(getLikeUseCase().getOrNull() ?: arrayListOf())
+    }
 }
